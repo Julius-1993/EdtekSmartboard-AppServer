1bc211bfa0d3e801a8e0d0212440b0db9b824586
Julius Aako <aakojuiusoluwanifemi@gmail.com>
2026-03-07 13:35:22 +0100

    cors blocking debug

--- api/index.js
+++ api/index.js
@@ -19,7 +19,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,}));
 app.use(express.json());
 app.use(bodyParser.json());
 
