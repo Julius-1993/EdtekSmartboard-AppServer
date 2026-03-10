b0f9c0bd97ae83b726e38d7d98ef7312cca6dff2
Julius Aako <aakojuiusoluwanifemi@gmail.com>
2026-03-10 18:00:56 +0100

    more correction

--- api/models/User.js
+++ api/models/User.js
@@ -9,6 +9,13 @@ const userSchema = new mongoose.Schema({
         minlength: 3
     },
     photoURL: String,
+    firebaseUID:{
+        type: String,
+    },
+    address:{
+        type: String
+    },
+    
     role: {
         type: String,
         enum: ['user', 'admin'],
